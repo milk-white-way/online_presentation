db5a66e184feee7c2aa334a4f1d8894b0ddf363d
milk-white-way <tam.thien.nguyen@ndsu.edu>
2026-06-03 15:41:33 -0500

    Initial slidev setup

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  description = "Slidev presentation";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = with pkgs; [ nodejs pnpm ];
+      };
+    };
+}
